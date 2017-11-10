f6c6dedaa684d822ad71bfe8ca99e5987f248fe1
Ivan Patsai <ivan.patsai@binary-studio.com>
2017-11-10 18:36:33 +0200

    add mock for history table

--- server/boot/mock_data.js
+++ server/boot/mock_data.js
@@ -4,6 +4,7 @@ module.exports = function (app) {
   postgresDb.automigrate('Todo', function (err) {
     if (err) throw err
     var Todo = app.models.Todo
+    var TodoHistory = app.models.Todo_History
     Todo.create([
         {
           name: 'Todo 1',
@@ -16,6 +17,32 @@ module.exports = function (app) {
         },
       ]
     )
+    TodoHistory.create([
+      {
+        name: "update 1",
+        todoId: 1
+      },
+      {
+        name: "update 2",
+        todoId: 1
+      },
+      {
+        name: "update 3",
+        todoId: 1
+      },
+      {
+        name: "update 12",
+        todoId: 2
+      },
+      {
+        name: "update 22",
+        todoId: 2
+      },
+      {
+        name: "update 32",
+        todoId: 2
+      }
+    ])
   })
   console.log('data created')
 }
