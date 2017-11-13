ac1f94daefe9530477c22ec43a4ebd903b7bb029
Ivan Patsai <ivan.patsai@binary-studio.com>
2017-11-13 18:25:30 +0200

    set data source regarding to process.env variable

--- server/boot/mock_data.js
+++ server/boot/mock_data.js
@@ -1,25 +1,14 @@
 module.exports = function (app) {
-  var postgresDb = app.dataSources.postgres
-  var mongoDb = app.dataSources.mongodb
+  var dataSource
+  if (process.env.DATA_SOURCE) {
+    app.models.Todo.attachTo(app.dataSources[process.env.DATA_SOURCE])
+    app.models.Todo_History.attachTo(app.dataSources[process.env.DATA_SOURCE])
+    dataSource = app.dataSources[process.env.DATA_SOURCE]
+  } else {
+    dataSource = app.dataSources.postgres
+  }
 
-  // postgresDb.automigrate('Todo', function (err) {
-  //   if (err) throw err
-  //   var Todo = app.models.Todo
-  //   Todo.create([
-  //       {
-  //         name: 'Todo 1',
-  //       },
-  //       {
-  //         name: 'Todo 2',
-  //       },
-  //       {
-  //         name: 'Todo 3',
-  //       },
-  //     ]
-  //   )
-  //   console.log('Todo created')
-  // })
-  mongoDb.automigrate('Todo', function (err) {
+  dataSource.automigrate('Todo', function (err) {
     if (err) throw err
     var Todo = app.models.Todo
     Todo.create([
@@ -36,38 +25,8 @@ module.exports = function (app) {
     )
     console.log('Todo created')
   })
-  // postgresDb.automigrate('Todo_History', function (err) {
-  //   if (err) throw err
-  //   var TodoHistory = app.models.Todo_History
-  //   TodoHistory.create([
-  //     {
-  //       name: 'update 1',
-  //       todoId: 1,
-  //     },
-  //     {
-  //       name: 'update 2',
-  //       todoId: 1,
-  //     },
-  //     {
-  //       name: 'update 3',
-  //       todoId: 1,
-  //     },
-  //     {
-  //       name: 'update 12',
-  //       todoId: 2,
-  //     },
-  //     {
-  //       name: 'update 22',
-  //       todoId: 2,
-  //     },
-  //     {
-  //       name: 'update 32',
-  //       todoId: 2,
-  //     },
-  //   ])
-  //   console.log('Todo_History created')
-  // })
-  mongoDb.automigrate('Todo_History', function (err) {
+
+  dataSource.automigrate('Todo_History', function (err) {
     if (err) throw err
     var TodoHistory = app.models.Todo_History
     TodoHistory.create([
