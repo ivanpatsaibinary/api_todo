fce449bf98f7e722072e72b94633bbc2d7a46c30
Ivan Patsai <ivan.patsai@binary-studio.com>
2017-11-14 10:58:16 +0200

    add todos numbers

--- server/boot/mock_data.js
+++ server/boot/mock_data.js
@@ -22,64 +22,64 @@ module.exports = function (app) {
           name: 'Todo 3',
         },
         {
-          name: 'Todo 2',
+          name: 'Todo 4',
         },
         {
-          name: 'Todo 3',
+          name: 'Todo 5',
         },
         {
-          name: 'Todo 2',
+          name: 'Todo 6',
         },
         {
-          name: 'Todo 3',
+          name: 'Todo 7',
         },
         {
-          name: 'Todo 2',
+          name: 'Todo 8',
         },
         {
-          name: 'Todo 3',
+          name: 'Todo 9',
         },
         {
-          name: 'Todo 2',
+          name: 'Todo 10',
         },
         {
-          name: 'Todo 3',
+          name: 'Todo 11',
         },
         {
-          name: 'Todo 2',
+          name: 'Todo 12',
         },
         {
-          name: 'Todo 3',
+          name: 'Todo 13',
         },
         {
-          name: 'Todo 2',
+          name: 'Todo 14',
         },
         {
-          name: 'Todo 3',
+          name: 'Todo 15',
         },
         {
-          name: 'Todo 2',
+          name: 'Todo 16',
         },
         {
-          name: 'Todo 3',
+          name: 'Todo 17',
         },
         {
-          name: 'Todo 2',
+          name: 'Todo 18',
         },
         {
-          name: 'Todo 3',
+          name: 'Todo 19',
         },
         {
-          name: 'Todo 2',
+          name: 'Todo 20',
         },
         {
-          name: 'Todo 3',
+          name: 'Todo 21',
         },
         {
-          name: 'Todo 2',
+          name: 'Todo 22',
         },
         {
-          name: 'Todo 3',
+          name: 'Todo 23',
         },
       ]
     )
